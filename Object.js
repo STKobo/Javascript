5f09b43e0bfcdbf9e6e6a7667c5be509f288b741
STKobo <95635784+STKobo@users.noreply.github.com>
2022-08-04 17:38:03 +0200

    Create Object.js

---
+++ Object.js
@@ -0,0 +1,12 @@
+let plant = {
+  name: 'Pachira aquatica',
+  family: 'Malvaceaae',
+  size: '140cm',
+  origin: 'Amerique du sud',
+}
+
+console.log(Object.keys(plant));
+console.log(Object.values(plant));
+console.log(Object.entries(plant));
+
+//affiche les propriétés et les valeurs 
