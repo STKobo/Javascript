4890371c575a931ae2b72d8875b7a2ed2b893189
Stephane <stephanetcha@gmail.com>
2022-08-10 19:16:15 +0200

    event affiche un dialogue pour chaque noeud du DOM

---
+++ ProgEvent/phases/script.js
@@ -0,0 +1,6 @@
+for (let element of document.querySelectorAll('*')) {
+    element.addEventListener("click", (event) => {
+      event.stopPropagation();
+      alert(` Capturing the element: ${element.tagName}`)
+    }, false);
+ }
