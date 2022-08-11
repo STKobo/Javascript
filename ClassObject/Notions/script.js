794ec65800cabf3a0779864bb0e400880f5633d8
Stephane <stephanetcha@gmail.com>
2022-08-11 19:04:16 +0200

    notions sur les classes en JS

---
+++ ClassObject/Notions/script.js
@@ -0,0 +1,32 @@
+
+ // Version ES5
+	     function Personne(nom, prenom, age, sexe, adresse) {
+	
+		    this.nom = nom
+		    this.prenom = prenom
+		    this.age = age
+		    this.sexe = sexe
+		    this.adresse = adresse
+ 
+ 
+		}
+		
+ // Version ES6
+		class Humain {
+  		constructor(nom, prenom, age, sexe, adresse) {
+	    	this.nom = nom
+		    this.prenom = prenom
+		    this.age = age
+		    this.sexe = sexe
+		    this.adresse = adresse
+ 
+			}
+ 
+			
+		}
+		
+		let personne1 = new Personne()
+		console.log(personne1)
+		
+		let personne2 = new Humain()
+		console.log(personne2)
