c409adba5ed84869e1615d58a4c0ad925208d4ad
Stephane <stephanetcha@gmail.com>
2022-08-12 14:47:03 +0200

    sélecteur jquery

---
+++ jQuery/Sélecteurs/script.js
@@ -0,0 +1,7 @@
+$(document).ready(() => {
+    $('h1, h2, .content').css('color', 'green');
+  });
+
+  $(document).ready(() => {
+    $('p.content:last-of-type').css('font-style', 'italic');
+  });
