38f69d2aaa625ac4c8601ab0fda69da2a6d7785c
Stephane <stephanetcha@gmail.com>
2022-08-12 15:25:37 +0200

    event avec jQuery

---
+++ jQuery/elementInteraction/script.js
@@ -0,0 +1,16 @@
+$(document).ready(() => {
+    $('#introduction-copy').html($('#introduction').html())
+});
+
+$(document).ready(() => {
+    let goals = $( "#goals" )
+    $.data( goals, "goals", {
+      0 : "Récupérer le code HTML",
+      1 : "Modifier le CSS",
+      2 : "Parcourir une liste d'éléments",
+      3 : "Associer des données à un élément"
+    })
+    $('#goals li').each(function(index) {
+      $(this).text($.data( goals, "goals" )[index])
+    })
+  });
