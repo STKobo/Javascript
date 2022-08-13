32bb1ead678e232d401bf1cb25cdc7acfe8dc59b
Stephane <stephanetcha@gmail.com>
2022-08-13 18:17:26 +0200

    les promesses

---
+++ Promise/1_Notions/script.js
@@ -0,0 +1,20 @@
+function askAge() {
+    return prompt('Quel âge avez-vous ?')
+  }   
+  function success() {
+    console.log('Vous êtes majeur, vous pouvez continuer votre navigation')
+  }
+  function error() {
+    console.log('Vous êtes mineur, vous allez être redirigé vers une autre page')
+  }
+  function redirectUser() {
+    return new Promise((resolve, reject) => {
+      let age = askAge()
+      if (age >= 18) {
+        resolve()
+      } else {
+        reject()
+      }
+    })
+  }
+  redirectUser().then(success, error)
