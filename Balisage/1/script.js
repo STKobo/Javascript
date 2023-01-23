3be7d864a642da7e26845be068e7247496fb39c3
Stephane_tcha <stephanetcha@gmail.com>
2023-01-23 15:01:29 +0100

    remplacer un texte

--- Balisage/1/script.js
+++ Balisage/1/script.js
@@ -24,3 +24,14 @@ function utiliseLesSelecteurs(){
     let querySelector = document.querySelector('p.i.maClasse');
     console.log(querySelector);
 }
+
+function remplaceLeTexte() {
+    //récupérer le texte dans l'input 
+    let textInput = document.getElementById("leTexteARecuperer").value;
+
+    //récupérer le noeud
+    let monParagraphe = document.querySelector('#leTexteARemplacer');
+
+    //mettre le texte de l'input dans le noeud 
+    monParagraphe.textContent = textInput; 
+}
